eaeb33b494f85e124373890f8a8072b293ce320f
Nandakishore <2483398@cognizant.com>
2026-03-25 12:09:02 +0530

    added data folder

---
+++ FarmManagement.Web/Data/FarmDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FarmManagement.Web.Data
+{
+    public class FarmDbContext : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
