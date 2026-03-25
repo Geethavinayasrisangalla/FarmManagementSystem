bb98d8087f8a61fa6a80adb5355bb2c65f548139
Nandakishore <2483398@cognizant.com>
2026-03-25 11:57:36 +0530

    added controllers and views

---
+++ FarmManagement.Web/Controllers/ResourceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FarmManagement.Web.Controllers
+{
+    public class ResourceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
